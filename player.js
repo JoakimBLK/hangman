6097495e6e6a165c3cd7bbfe53bd64c7596428b4
Joakim <joakim.kvistholm@gmail.com>
2023-11-03 20:51:14 +0100

    Added the class player.js to project with get and set functions for the name and score variables. Also creates a string of the player to write to a file.

---
+++ player.js
@@ -0,0 +1,43 @@
+
+
+import PromptSync from 'prompt-sync';
+
+const prompt = PromptSync({ sigint: true });
+
+const print = console.log;
+
+export default class Player {
+
+  name;
+  score;
+
+  constructor(name) {
+    score = 0;
+    this.name = name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setScore(score) {
+    this.score = score;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  asStringForFile() {
+
+    let strText = "";
+    strText = strText + this.getName() + "," + this.getScore() + "\r\n";
+    return strText;
+
+  }
+
+}
