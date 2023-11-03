68ed5bd7bc765a921e62c6052f146b73ecc159b4
Joakim <joakim.kvistholm@gmail.com>
2023-11-03 23:01:33 +0100

    Working with filehandler.js - add the method to read from file - not finnished yet.

--- filehandler.js
+++ filehandler.js
@@ -5,10 +5,15 @@ const prompt = PromptSync({ sigint: true });
 
 const print = console.log;
 
+import { appendFileSync, readFileSync } from 'node:fs';
+
+
 export default class FileHandler {
 
   txtFileWord;
   txtFileHighScore;
+  players = [];
+  words = [];
 
   constructor() {
     this.txtFileWord = "data/correctwords.csv";
@@ -31,4 +36,29 @@ export default class FileHandler {
     return this.txtFileHighScore;
   }
 
+  getHighScoresFromFile() {
+
+    let savedPlayers = [];
+
+    try {
+      savedPlayers = readFileSync(this.txtFileHighScore, "utf-8");
+    } catch (err) {
+      console.error(err);
+    }
+
+    savedPlayers = savedPlayers.trim();
+    savedPlayers = savedPlayers.split('\r\n');
+
+
+
+    for (let player of savedPlayers) {
+      player = player.split(',');
+      this.players.push(new Player(user[0], user[1]));
+    }
+
+    console.log(users);
+
+    return this.players;
+
+  }
 }
